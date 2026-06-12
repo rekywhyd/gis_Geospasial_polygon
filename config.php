1fce1123bb746cfdd4d1c97270403ea06cac2d53
rekywhyd <rekywahyudi00@gmail.com>
2026-06-13 04:15:54 +0700

    menambah getenv

--- config.php
+++ config.php
@@ -1,4 +1,9 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "gis_polygon");
+$db_host = getenv('DB_HOST') ?: 'localhost';
+$db_user = getenv('DB_USER') ?: 'root';
+$db_pass = getenv('DB_PASS') ?: '';
+$db_name = getenv('DB_NAME') ?: 'gis_polygon';
+
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 if (!$conn) { die("Koneksi gagal: " . mysqli_connect_error()); }
 ?>
